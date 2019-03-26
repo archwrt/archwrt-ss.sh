58b072d812a53a8a0e8fec1186f859560681af5e
edward-p <edward@edward-p.xyz>
2019-03-26 15:09:59 +0800

    gamemode: support udp redirect on localhost

--- archwrt-ss.sh
+++ archwrt-ss.sh
@@ -181,7 +181,7 @@ config_ipset() {
 	ip_tg="149.154.0.0/16 91.108.4.0/22 91.108.56.0/24 109.239.140.0/24 67.198.55.0/24"
 	for ip in ${ip_tg}
 	do
-		ipset -! add black_list ${ip} >/dev/null 2>&1
+		ipset -! add black_list ${ip} &>/dev/null
 	done
 
 	ss_server_ip=$(cat ${ss_config} | grep server | grep -oE "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
@@ -190,7 +190,7 @@ config_ipset() {
 	ip_lan="0.0.0.0/8 10.0.0.0/8 100.64.0.0/10 127.0.0.0/8 169.254.0.0/16 172.16.0.0/12 192.168.0.0/16 224.0.0.0/4 240.0.0.0/4 ${ss_server_ip} ${china_dns} 223.5.5.5 223.6.6.6 114.114.114.114 114.114.115.115 1.2.4.8 210.2.4.8 112.124.47.27 114.215.126.16 180.76.76.76 119.29.29.29"
 	for ip in ${ip_lan}
 	do
-		ipset -! add white_list ${ip} >/dev/null 2>&1
+		ipset -! add white_list ${ip} &>/dev/null
 	done
 	# add custom black list
 	cat /dev/null > /etc/dnsmasq.d/20-wblist_ipset.conf
@@ -260,25 +260,26 @@ create_nat_rules() {
 		iptables -t mangle -A SHADOWSOCKS -p udp -m set --match-set white_list dst -j RETURN
 		iptables -t mangle -A SHADOWSOCKS -p udp -m set --match-set black_list dst -j TPROXY --on-port "${local_port}" --tproxy-mark 0x07
 		iptables -t mangle -A SHADOWSOCKS -p udp -m set ! --match-set bypass dst -j TPROXY --on-port "${local_port}" --tproxy-mark 0x07
+		
+		iptables -t mangle -N SS_OUTPUT
+		iptables -t mangle -A SS_OUTPUT -p udp -j RETURN -m set --match-set white_list dst
+		iptables -t mangle -A SS_OUTPUT -p udp -m set --match-set black_list dst -j MARK --set-mark 0x07
+		iptables -t mangle -A SS_OUTPUT -p udp -m set ! --match-set bypass dst -j MARK --set-mark 0x07
+
 	else
 		echo "Wrong proxy mode!" && exit 1
 	fi
-	# chromecast
-	[ -n "${lanip}" ] && iptables -t nat -I PREROUTING -s ${lanip}/24 -p udp --dport 53 -m comment --comment "ss_chromecast" -j DNAT --to ${lanip}
+
+	#PREROUTING
+
+	# dns_redir
+	[ -n "${lanip}" ] && iptables -t nat -I PREROUTING -s ${lanip}/24 -p udp --dport 53 -m comment --comment "dns_redir" -j DNAT --to ${lanip}
 	# forward
 	iptables -t nat -I PREROUTING 1 -p tcp -j SHADOWSOCKS
 	[ "${ss_mode}" = "gamemode" ] && iptables -t mangle -I PREROUTING 1 -p udp -j SHADOWSOCKS
 	# for router self
 	iptables -t nat -I OUTPUT 1 -p tcp -j SHADOWSOCKS
-	# add iptables config
-	cat /dev/null > /etc/iptables/shadowsocks.rules
-	iptables -t nat -S SHADOWSOCKS | grep SHADOWSOCKS >> /etc/iptables/shadowsocks.rules
-	iptables -t nat -S PREROUTING | grep -E 'SHADOWSOCKS|chromecast' >> /etc/iptables/shadowsocks.rules
-	iptables -t nat -S OUTPUT | grep SHADOWSOCKS >> /etc/iptables/shadowsocks.rules
-	if [ "${ss_mode}" = "gamemode" ]; then
-		iptables -t mangle -S SHADOWSOCKS | grep SHADOWSOCKS >> /etc/iptables/shadowsocks.rules
-		iptables -t mangle -S PREROUTING | grep SHADOWSOCKS >> /etc/iptables/shadowsocks.rules
-	fi
+	[ "${ss_mode}" = "gamemode" ] && iptables -t mangle -I OUTPUT 1 -p udp -j SS_OUTPUT
 
 }
 
@@ -293,19 +294,21 @@ flush_nat() {
 	echo "Clear rules..."
 	# flush shadowsocks rules
 	eval "`iptables -t nat -S | grep SHADOWSOCKS | sed 1d | sed -e "s/-A/iptables -t nat -D/"`"
-	iptables -t nat -D PREROUTING -p tcp -j SHADOWSOCKS >/dev/null 2>&1
+	iptables -t nat -D PREROUTING -p tcp -j SHADOWSOCKS &>/dev/null
+	iptables -t mangle -D OUTPUT -p udp -j SS_OUTPUT
 	iptables -t nat -F SHADOWSOCKS > /dev/null 2>&1 && iptables -t nat -X SHADOWSOCKS > /dev/null 2>&1
 	eval "`iptables -t mangle -S | grep SHADOWSOCKS | sed 1d | sed -e "s/-A/iptables -t mangle -D/"`"
-	iptables -t mangle -D PREROUTING -p udp -j SHADOWSOCKS >/dev/null 2>&1
-	iptables -t mangle -F SHADOWSOCKS >/dev/null 2>&1 && iptables -t mangle -X SHADOWSOCKS >/dev/null 2>&1
+	iptables -t mangle -D PREROUTING -p udp -j SHADOWSOCKS &>/dev/null
+	iptables -t mangle -F SHADOWSOCKS &>/dev/null && iptables -t mangle -X SHADOWSOCKS &>/dev/null
+	iptables -t mangle -F SS_OUTPUT &>/dev/null && iptables -t mangle -X SS_OUTPUT &>/dev/null
 	iptables -t nat -D OUTPUT -p tcp -j SHADOWSOCKS &> /dev/null
-	# flush chromecast rule
-	eval `iptables -t nat -S | grep "ss_chromecast" | head -1 | sed -e "s/-A/iptables -t nat -D/"` &> /dev/null
+	# flush dns_redir rule
+	eval `iptables -t nat -S | grep "dns_redir" | head -1 | sed -e "s/-A/iptables -t nat -D/"` &> /dev/null
 	# flush ipset
-	ipset -F bypass >/dev/null 2>&1 && ipset -X bypass >/dev/null 2>&1
-	ipset -F white_list >/dev/null 2>&1 && ipset -X white_list >/dev/null 2>&1
-	ipset -F black_list >/dev/null 2>&1 && ipset -X black_list >/dev/null 2>&1
-	ipset -F gfwlist >/dev/null 2>&1 && ipset -X gfwlist >/dev/null 2>&1
+	ipset -F bypass &>/dev/null && ipset -X bypass &>/dev/null
+	ipset -F white_list &>/dev/null && ipset -X white_list &>/dev/null
+	ipset -F black_list &>/dev/null && ipset -X black_list &>/dev/null
+	ipset -F gfwlist &>/dev/null && ipset -X gfwlist &>/dev/null
 	# remove_redundant_rule
 	ip_rule_exist=`ip rule show | grep "fwmark 0x7 lookup 310" | grep -c 310`
 	if [ ! -z "ip_rule_exist" ];then
@@ -319,14 +322,12 @@ flush_nat() {
 	fi
 	# remove_route_table
 	echo "Clearing rules..."
-	ip route del local 0.0.0.0/0 dev lo table 310 >/dev/null 2>&1
+	ip route del local 0.0.0.0/0 dev lo table 310 &>/dev/null
 	# remove dnsmasq config
 	rm -rf /etc/dnsmasq.d/10-dns.conf
 	rm -rf /etc/dnsmasq.d/20-gfwlist_ipset.conf
 	rm -rf /etc/dnsmasq.d/20-sscdn_ipset.conf
 	rm -rf /etc/dnsmasq.d/20-wblist_ipset.conf
-	# remove iptables config
-	rm -rf /etc/iptables/shadowsocks.rules
 }
 
 restart_dnsmasq() {
