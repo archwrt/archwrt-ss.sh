f17cac7be2410f47d2d574c1147f75e11a161285
edward-p <edward@edward-p.xyz>
2021-03-07 20:12:37 +0800

    remove comments in chnroute.txt

--- archwrt-ss.sh
+++ archwrt-ss.sh
@@ -129,7 +129,7 @@ update_rules() {
 
 	if [ ! -f "${chnroute}" ] || [ "$1" = "f" ]; then
 		echo "Downloading chnroute.txt..."
-		! curl -kLo /tmp/chnroute.txt "https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/ipip_country/ipip_country_cn.netset" &&
+		! curl -kL "https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/ipip_country/ipip_country_cn.netset" | grep -v '^#' > /tmp/chnroute.txt &&
 			echo "Download failed! Check your connection!" && exit 1
 		install -D -m644 /tmp/chnroute.txt "${chnroute}" &>/dev/null
 		rm /tmp/chnroute.txt
